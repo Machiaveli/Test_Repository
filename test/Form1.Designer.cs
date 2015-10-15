683659b13e8c1c3718bf481bbbde3036bc954d21
yusufbhyat@yahoo.com.au <5c0ug@r5>
2015-10-15 16:57:14 +1000

    updated title

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -103,7 +103,7 @@
             this.Controls.Add(this.lblTwo);
             this.Controls.Add(this.lblOne);
             this.Name = "Form1";
-            this.Text = "Encryption Tool";
+            this.Text = "Encryption Tool - v2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
