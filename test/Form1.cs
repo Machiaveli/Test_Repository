e6a4890adcd888f5481fdfc146fcb0458894333d
eyehgc - Eunice Yeh <eunice422@gmail.com>
2015-10-18 17:52:35 +1000

    test button - eunice

--- test/Form1.cs
+++ test/Form1.cs
@@ -87,5 +87,10 @@ namespace test
                 txtEncrypted.Text = encryptString(txtToEncrypt.Text);
         }
 
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi!","Test");
+        }
+
     }
 }
